0e2c274f16cee54b48905fb4b14fd00de09fad0f
Mikhail Sidarkevich <mikhail_sidarkevich@epam.com>
2022-10-22 22:16:35 +0300

    update migration

--- prisma/migrations/20221022184857_checked/migration.sql
+++ prisma/migrations/20221022184857_checked/migration.sql
@@ -1,10 +1,1 @@
--- CreateTable
-CREATE TABLE "Todo" (
-    "id" TEXT NOT NULL,
-    "title" TEXT NOT NULL,
-    "checked" BOOLEAN NOT NULL DEFAULT false,
-    "createdAt" TIMESTAMP(3) NOT NULL DEFAULT CURRENT_TIMESTAMP,
-    "updatedAt" TIMESTAMP(3) NOT NULL,
-
-    CONSTRAINT "Todo_pkey" PRIMARY KEY ("id")
-);
+ALTER TABLE "public"."Todo" DROP COLUMN checked -- fast and safe
