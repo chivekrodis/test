0f5a889791f28816d0bc7c690002af0ac15de6de
Mikhail Sidarkevich <mikhail_sidarkevich@epam.com>
2022-10-22 21:32:31 +0300

    fix

--- src/components/button/index.tsx
+++ src/components/button/index.tsx
@@ -1,15 +1,17 @@
-import { FC } from "react"
+import { FC } from 'react'
 
 interface ButtonProps {
-    handler: () => void,
-    label: String
+  handler: () => void
+  label: string
 }
 
 export const Button: FC<ButtonProps> = ({ handler, label }) => {
-    return <button
-        className="rounded-md border border-transparent bg-indigo-600 py-2 px-4 text-sm font-medium text-white hover:bg-indigo-700 focus:outline-none focus:ring-2 focus:ring-indigo-500 focus:ring-offset-2"
-        onClick={handler}
+  return (
+    <button
+      className="rounded-md border border-transparent bg-indigo-600 py-2 px-4 text-sm font-medium text-white hover:bg-indigo-700 focus:outline-none focus:ring-2 focus:ring-indigo-500 focus:ring-offset-2"
+      onClick={handler}
     >
-        {label}
+      {label}
     </button>
+  )
 }
